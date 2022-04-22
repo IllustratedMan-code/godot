f7f21eabb92d717acac2e09b9a934f0f7bfe76cb
David Lewis <davidalewis00@gmail.com>
2022-04-22 12:48:51 -0400

    added glu libs

--- platform/linuxbsd/detect.py
+++ platform/linuxbsd/detect.py
@@ -227,7 +227,8 @@ def configure(env):
     env.ParseConfig("pkg-config xrender --cflags --libs")
     env.ParseConfig("pkg-config xi --cflags --libs")
     env.ParseConfig("pkg-config xfixes --cflags")
-    env.ParseConfig("pkg-config glu --cflags")
+    env.ParseConfig("pkg-config glu --cflags --libs")
+    
 
 
     if env["touch"]:
